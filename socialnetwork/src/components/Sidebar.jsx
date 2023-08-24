545d0c7616dfa9af4b04214d7c6477d8c360d4b3
JonatasAS <jonatas23997@live.com>
2023-08-23 22:15:45 -0300

    component avatar

--- socialnetwork/src/components/Sidebar.jsx
+++ socialnetwork/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from '@phosphor-icons/react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -9,7 +10,7 @@ export function Sidebar() {
       <img className={styles.cover} src='https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60' />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/JonatasAS.png" alt="" />
+        <Avatar src="https://github.com/JonatasAS.png"/>
 
         <strong>Jonatas Amorim</strong>
         <span>Web Developer</span>
